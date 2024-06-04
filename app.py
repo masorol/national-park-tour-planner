8ba76870a786268fa01cf597f88b6d374fd0d87d
lsavoie <twosavoie@gmail.com>
2024-06-03 23:51:07 -0500

    Add solution for prompt template challenge

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 import logging
 import datetime
+from langchain_core.prompts import PromptTemplate
 
 # app will run at: http://127.0.0.1:5000/
 
-# set up logging in the assistant.log file
 log = logging.getLogger("app")
 
 logging.basicConfig(filename = "app.log", level = logging.INFO)
@@ -15,6 +15,19 @@ app = Flask(__name__)
 def log_run(run_status):
     if run_status in ["cancelled", "failed", "expired"]:
         log.error(str(datetime.datetime.now()) + " Run " + run_status + "\n")
+        
+def build_new_trip_prompt(form_data):
+  prompt_template = PromptTemplate.from_template("This trip is to {location} between {trip_start} and {trip_end}. This person will be traveling {traveling_with} and would like to stay in {lodging}. They want to {adventure}. Create a daily itinerary for this trip using this information.")
+  
+  return prompt_template.format(
+    location = form_data["location"],
+    trip_start = form_data["trip_start"],
+    trip_end = form_data["trip_end"],
+    traveling_with = form_data["traveling_with"],
+    lodging = form_data["lodging"],
+    adventure = form_data["adventure"]
+    )
+
 
 # Render the HTML template - we're going to see a UI!!!
 @app.route("/", methods=["GET"])
@@ -27,8 +40,24 @@ def plan_trip():
 
 @app.route("/create_trip", methods=["POST"])
 def create_trip():
+  traveling_with_list = ", ".join(request.form.getlist("traveling-with"))
+  lodging_list = ", ".join(request.form.getlist("lodging"))
+  adventure_list = ", ".join(request.form.getlist("adventure"))
+  
+  cleaned_form_data = {
+    "location": request.form["location-search"],
+    "trip_start": request.form["trip-start"],
+    "trip_end": request.form["trip-end"],
+    "traveling_with": traveling_with_list,
+    "lodging": lodging_list,
+    "adventure": adventure_list
+    }
+  
+  # log.info(cleaned_form_data)
+  # print(cleaned_form_data)
+  # prompt = build_new_trip_prompt(cleaned_form_data)
+  # log.info(prompt)
   return render_template("create-trip.html")
-
 
 
     
