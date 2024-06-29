6b7465567e8a4b760cfb2a29b0a470caea917409
lsavoie <twosavoie@gmail.com>
2024-06-29 12:56:32 -0500

    Add url to app.py file

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from langchain_community.utilities import WikipediaAPIWrapper
 from langchain.tools import StructuredTool
 from langchain import hub
 
+# app will run at: http://127.0.0.1:5000/
+
 # Initialize logging
 logging.basicConfig(filename="app.log", level=logging.INFO)
 log = logging.getLogger("app")
