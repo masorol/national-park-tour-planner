621fe05be1f35e7b3913eeded8d5cd24602156ab
lsavoie <twosavoie@gmail.com>
2024-07-23 12:05:42 -0500

    Updates to be consistent with completed project

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import logging
-import datetime
+from datetime import datetime
 
 # app will run at: http://127.0.0.1:5000/
 
@@ -8,28 +8,23 @@ import datetime
 logging.basicConfig(filename="app.log", level=logging.INFO)
 log = logging.getLogger("app")
 
+# Initialize the Flask application
 app = Flask(__name__)
-        
-def log_run(run_status):
-    """Logs the status of a run if it is cancelled, failed, or expired."""
-    if run_status in ["cancelled", "failed", "expired"]:
-        log.error(f"{datetime.datetime.now()} Run {run_status}\n")
 
-# Render the HTML template - we're going to see a UI!!!
+# Define the route for the home page
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
   
+# Define the route for the plan trip page
 @app.route("/plan_trip", methods=["GET"])
 def plan_trip():
   return render_template("plan-trip.html")
 
+# Define the route for view trip page with the generated trip itinerary
 @app.route("/view_trip", methods=["POST"])
 def view_trip():
   return render_template("view-trip.html")
-
-
-
     
 # Run the flask server
 if __name__ == "__main__":
